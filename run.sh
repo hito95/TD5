9c4acb5eb19738531c2e5af4cffd5ba73e14f60b
MarwanM1 <59145104+MarwanM1@users.noreply.github.com>
2021-03-09 17:14:48 +0100

    Script run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-virtualenv -q -p /usr/bin/python3.8 $1
-source $1/bin/activate
-$1/bin/pip install -r requirements.txt
+python3 -m pip install virtualenv 
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
