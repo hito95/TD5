8bf78f1d88c9643b8fc4094f7ec61f93c522a935
MarwanM1 <59145104+MarwanM1@users.noreply.github.com>
2021-03-10 11:27:45 +0100

    Check if the directory exists

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-python3 -m pip install virtualenv 
-virtualenv env
+if [ -d "$PWD/env" ]; then
+    echo "$PWD/env exists."
+else 
+    echo "$PWD/env does not exist."
+    python3 -m pip install virtualenv
+    virtualenv env
+fi
 source env/bin/activate
 pip install -r requirements.txt
 python3 main.py
